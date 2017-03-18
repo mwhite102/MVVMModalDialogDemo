10ea09b8f14e47acaa9ed57dbb20c364e6df403d
mwhite102 <mwhite102@gmail.com>
2017-03-18 13:00:10 -0500

    Basic GUI Added

---
+++ MVVMModalDialogDemo/Models/PersonListModel.cs
@@ -0,0 +1,25 @@
+﻿using MVVMModalDialogDemo.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVVMModalDialogDemo.Models
+{
+    public class PersonListModel
+    {
+        public PersonListModel(Person person)
+        {
+            this.Id = person.Id;
+            this.FirstName = person.FirstName;
+            this.LastName = person.LastName;
+            this.BirthDate = person.BirthDate;
+        }
+
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime? BirthDate { get; set; }
+    }
+}
--- MVVMModalDialogDemo/ViewModel/MainViewModel.cs
+++ MVVMModalDialogDemo/ViewModel/MainViewModel.cs
@@ -1,5 +1,11 @@
 using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.CommandWpf;
 using MVVMModalDialogDemo.DataService;
+using System.Windows.Input;
+using System;
+using MVVMModalDialogDemo.Models;
+using System.Collections.ObjectModel;
+using System.Threading.Tasks;
 
 namespace MVVMModalDialogDemo.ViewModel
 {
@@ -15,5 +21,135 @@ namespace MVVMModalDialogDemo.ViewModel
         {
             _DataService = dataService;
         }
+
+        public ObservableCollection<PersonListModel> People { get; } = new ObservableCollection<PersonListModel>();
+
+        private PersonListModel _SelectedPersonListModel;
+
+        public PersonListModel SelectedPersonListModel
+        {
+            get
+            {
+                return _SelectedPersonListModel;
+            }
+            set
+            {
+                if (_SelectedPersonListModel != value)
+                {
+                    _SelectedPersonListModel = value;
+                    RaisePropertyChanged();
+                }
+            }
+        }
+
+        #region Private Methods
+
+
+        private void DoNewPerson()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void DoPersonProperties()
+        {
+            throw new NotImplementedException();
+        }
+
+        private async Task Init()
+        {
+            var people = await _DataService.GetPeople();
+            foreach(var person in people)
+            {
+                this.People.Add(new PersonListModel(person));
+            }
+        }
+
+        #endregion
+
+        #region Commands
+
+        private ICommand _AddPersonCommand;
+
+        public ICommand AddPersonCommand
+        {
+            get
+            {
+                if (_AddPersonCommand == null)
+                {
+                    _AddPersonCommand = new RelayCommand(AddPersonCommandExecute);
+                }
+                return _AddPersonCommand;
+            }
+        }
+
+        private void AddPersonCommandExecute()
+        {
+            DoNewPerson();
+        }
+
+        private ICommand _EditPersonCommand;
+
+        public ICommand EditPersonCommand
+        {
+            get
+            {
+                if (_EditPersonCommand == null)
+                {
+                    _EditPersonCommand = new RelayCommand(EditPersonCommandExecute, EditPersonCommandCanExecute);
+                }
+                return _EditPersonCommand;
+            }
+        }
+
+        private bool EditPersonCommandCanExecute()
+        {
+            return _SelectedPersonListModel != null;
+        }
+
+        private void EditPersonCommandExecute()
+        {
+            DoPersonProperties();
+        }
+
+        private ICommand _ExitCommand;
+
+        public ICommand ExitCommand
+        {
+            get
+            {
+                if (_ExitCommand == null)
+                {
+                    _ExitCommand = new RelayCommand(ExitCommandExecute);
+                }
+                return _ExitCommand;
+            }
+        }
+
+        private void ExitCommandExecute()
+        {
+            App.Current.Shutdown(0);
+        }
+
+        private ICommand _InitCommand;
+
+        public ICommand InitCommand
+        {
+            get
+            {
+                if (_InitCommand == null)
+                {
+                    _InitCommand = new RelayCommand(InitCommandExecute);
+
+                }
+                return _InitCommand;
+            }
+        }
+
+        private async void InitCommandExecute()
+        {
+            await Init();
+        }
+
+        #endregion
     }
 }
